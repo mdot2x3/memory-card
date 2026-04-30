f0c7aa68edb980e319620a33e0744def4216e865
mdot2x3 <mdot2x3@gmail.com>
2026-04-29 17:49:52 -0700

    feat: create api fetch and shuffle functions
    in App.jsx:
    
    build an api fetch function using async/await with
    try/catch to filter and pull image urls from the
    api source inside a useEffect hook set to run
    once on mount.
    
    add a shuffle function that shuffles the array by
    swapping each element with a randomly selected
    element from the unshuffled portion.
    
    slice 12 items from the entire collection of
    shuffled image urls.
    
    add state tracking with the useState hook to store
    the fetched image url states.
    
    use .map in the component return section to
    render the images stored in state based on the url
    element and its index in the state array.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,48 @@
 import "./styles/App.css";
+import { useState, useEffect } from "react";
+
+function shuffle(array) {
+  // create a shallow copy (original unmodified)
+  const arr = [...array];
+  // move backwards from last index
+  for (let i = arr.length - 1; i > 0; i--) {
+    // (fisher-yates) picks a random index j between 0 and i (inclusive)
+    const j = Math.floor(Math.random() * (i + 1));
+    // shorthand array destructuring and assignment (no need for a temp variable)
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
 
 function App() {
+  const [imageSrc, setImageSrc] = useState([]);
+
+  useEffect(() => {
+    async function fetchImages() {
+      try {
+        const response = await fetch(
+          `https://jetsetradio-api.onrender.com/v1/api/graffiti-tags?size=S`,
+        );
+        const fetchData = await response.json();
+        console.log(fetchData);
+        const shuffled = shuffle(fetchData);
+        const selected = shuffled.slice(0, 12);
+        setImageSrc(selected.map((item) => item.imageUrl));
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchImages();
+  }, []);
+
   return (
     <>
       <h1>Memory Card Game</h1>
+      <div>
+        {imageSrc.map((src, idx) => (
+          <img key={idx} src={src} alt="" />
+        ))}
+      </div>
     </>
   );
 }
